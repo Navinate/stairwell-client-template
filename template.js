d2dc472af41b78d1e5b4bec45ddb4875ad4b5ac6
Navinate <treycluff@gmail.com>
2023-01-11 13:21:43 -0500

    updated template.js to match naming conventions

--- template.js
+++ template.js
@@ -5,6 +5,6 @@ var socket = io();
 // to recieve data from the backend
 // color is a hexadecimal color
 // a-e are numbers that range from 0 to 100
-socket.on("recieve visual data", (color, a, b, c, d, e) => {
+socket.on("server to listener", (color, a, b, c, d, e) => {
   //put whatever code should run whenever new data is pushed to your visual
 });
